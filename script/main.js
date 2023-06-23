67b91996b0e478753a11e44876a540ed74382533
walter <walaser80@gmail.com>
2023-06-23 18:31:55 -0300

    Beta v2

---
+++ script/main.js
@@ -0,0 +1,22 @@
+
+let submit = document.getElementById(`btn-submit`);
+let copy = document.getElementById(`copy`);
+
+const date = ()=>{
+    let today = new Date();
+    let year = today.getFullYear();
+    copy.innerHTML = `${year}`;
+}
+
+date();
+
+
+
+const send = () =>{
+    submit.addEventListener(`clikc`, ()=>{
+        Swal.fire("Enviado", "Su mensaje fue enviado correctamente", "success");
+    })
+}
+
+send();
+
